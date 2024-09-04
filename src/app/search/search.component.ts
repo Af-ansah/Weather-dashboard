2e6d1d5f9e5005191e00a091e4a3ed616d3f5202
Af-ansah <ansahgloria032@gmail.com>
2024-09-04 10:41:40 +0000

    feat:search on navbar

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,51 @@
+import { CommonModule } from '@angular/common';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SearchComponent),
+      multi: true,
+    },
+  ],
+})
+export class SearchComponent  implements ControlValueAccessor {
+  @Input() placeholder: string = '';
+  @Input() type: string = 'text';
+  @Input() isSelect: boolean = false;
+
+  value: string = '';
+  onChange: (value: string) => void = () => {};
+  onTouched: () => void = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  onInput(event: Event): void {
+    const target = event.target as HTMLInputElement | HTMLSelectElement;
+    this.value = target.value;
+    this.onChange(this.value);
+  }
+
+  onBlur(): void {
+    this.onTouched();
+  }
+
+
+}
